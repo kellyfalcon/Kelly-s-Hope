9a893e10ae97a667b314d8eecc2cf45332348ea9
kellyfalcon <kelly.falcon@ufl.edu>
2020-04-06 23:51:18 +0000

    First App

---
+++ my-app/build/precache-manifest.eda18b2a834368a22b5a50eb56052c39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658a94351111ca333709454b177c858e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea207f30bcf827e11f5f",
+    "url": "/static/css/main.850b3a0b.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea207f30bcf827e11f5f",
+    "url": "/static/js/main.b1822ef2.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "e13d4f7c086606cbdb698ecf58684a0f",
+    "url": "/static/media/heartgif.e13d4f7c.gif"
+  }
+]);
